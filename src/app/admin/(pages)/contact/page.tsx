0be95e347d77bb5bcc2420e9ac6550e31b661ede
nwalsh2102 <nwalsh0221@gmail.com>
2025-05-14 19:24:23 -0400

    FINAL CONTACT v14

--- src/app/admin/(pages)/contact/page.tsx
+++ src/app/admin/(pages)/contact/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import ContactTableHeader from "@/app/components/admin/contact/ContactTableHeader";
 import ContactTable from "@/app/components/admin/contact/ContactTable";
 
