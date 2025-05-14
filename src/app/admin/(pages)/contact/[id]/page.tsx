0bf75bdd7553929d615519ebdc7cf8fca9fe4211
nwalsh2102 <nwalsh0221@gmail.com>
2025-05-14 13:40:31 -0400

    FINAL CONTACT v8

--- src/app/admin/(pages)/contact/[id]/page.tsx
+++ src/app/admin/(pages)/contact/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function Page({
 }) {
   const id = await prisma.contactResponses.findUnique({
     where: {
-      id: Number(params.id),
+      id: params.id,
     },
   });
 
